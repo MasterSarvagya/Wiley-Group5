4d231b6410480f60a652ad193d31f933ef51355d
Jai-Pandey <pandeyjai00001@gmail.com>
2022-07-28 16:33:37 +0530

    Jai Presentation 1

--- WileyLibraryManagementSystem/src/main/java/Presentation/PresentationImpl.java
+++ WileyLibraryManagementSystem/src/main/java/Presentation/PresentationImpl.java
@@ -4,7 +4,19 @@ public class PresentationImpl implements Presentation {
 
 	@Override
 	public void showMenu() {
-		// TODO Auto-generated method stub
+		System.out.println(" 0.   Exit");
+		System.out.println(" 1.   Add An Employee");
+		System.out.println(" 2.   Delete An Employee");
+		System.out.println(" 3.   Search An Employee");
+		System.out.println(" 4.   Get All Employee");
+		System.out.println(" 5.   For Employee Login");
+		System.out.println(" 6.   Add A Book");
+		System.out.println(" 7.   Delete A Book");
+		System.out.println(" 8.   Search A Book");
+		System.out.println(" 9.   Get All Book");
+		System.out.println(" 10.  Issue A Book");
+		System.out.println(" 11.  Return A Book");
+		
 
 	}
 
